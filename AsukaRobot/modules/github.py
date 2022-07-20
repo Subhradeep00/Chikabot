0ef7780480bd8940af2bc66dae063b2f46d9f327
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-20 21:11:45 +0530

    Update github.py

--- AsukaRobot/modules/github.py
+++ AsukaRobot/modules/github.py
@@ -1,13 +1,13 @@
 import aiohttp
 from pyrogram import filters
-from AsukaRobot import pgram as pbot
-from AsukaRobot.utils.errors import capture_err
+from Akenobot import pgram as pbot
+from Akenobot.utils.errors import capture_err
 
 
 __mod_name__ = "Github"
 
 
-@pbot.on_message(filters.command(["github", "git", f"git@AsukaRobot"]))
+@pbot.on_message(filters.command(["github", "git", f"git@Akenobot"]))
 @capture_err
 async def github(_, message):
     if len(message.command) != 2:
