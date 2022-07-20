0884e9acc0e5ddd519cb908332e280cb4aed9429
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-20 21:23:34 +0530

    Update locks.py

--- AsukaRobot/modules/locks.py
+++ AsukaRobot/modules/locks.py
@@ -9,20 +9,20 @@ from telegram.utils.helpers import mention_html
 
 from alphabet_detector import AlphabetDetector
 
-import AsukaRobot.modules.sql.locks_sql as sql
-from AsukaRobot import dispatcher, DRAGONS, LOGGER
-from AsukaRobot.modules.disable import DisableAbleCommandHandler
-from AsukaRobot.modules.helper_funcs.chat_status import (
+import Akenobot.modules.sql.locks_sql as sql
+from Akenobot import dispatcher, DRAGONS, LOGGER
+from Akenobot.modules.disable import DisableAbleCommandHandler
+from Akenobot.modules.helper_funcs.chat_status import (
     can_delete,
     is_user_admin,
     user_not_admin,
     is_bot_admin,
     user_admin,
 )
-from AsukaRobot.modules.log_channel import loggable
-from AsukaRobot.modules.connection import connected
+from Akenobot.modules.log_channel import loggable
+from Akenobot.modules.connection import connected
 
-from AsukaRobot.modules.helper_funcs.alternate import send_message, typing_action
+from Akenobot.modules.helper_funcs.alternate import send_message, typing_action
 
 ad = AlphabetDetector()
 
