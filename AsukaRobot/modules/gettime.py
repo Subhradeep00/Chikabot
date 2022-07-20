0b57a98d4dcf6508274d2b2238ebcb727211e898
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-20 21:10:39 +0530

    Update gettime.py

--- AsukaRobot/modules/gettime.py
+++ AsukaRobot/modules/gettime.py
@@ -2,8 +2,8 @@ import datetime
 from typing import List
 
 import requests
-from AsukaRobot import TIME_API_KEY, dispatcher
-from AsukaRobot.modules.disable import DisableAbleCommandHandler
+from Akenobot import TIME_API_KEY, dispatcher
+from Akenobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
