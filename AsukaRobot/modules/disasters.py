a22ee059b0420f003a78d01830ce790a4e5f3c51
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-20 20:23:51 +0530

    Update disasters.py

--- AsukaRobot/modules/disasters.py
+++ AsukaRobot/modules/disasters.py
@@ -5,18 +5,18 @@ import json
 import os
 from typing import Optional
 
-from AsukaRobot import (DEV_USERS, OWNER_ID, DRAGONS, SUPPORT_CHAT, DEMONS,
+from Akenobot import (DEV_USERS, OWNER_ID, DRAGONS, SUPPORT_CHAT, DEMONS,
                           TIGERS, WOLVES, dispatcher)
-from AsukaRobot.modules.helper_funcs.chat_status import (dev_plus, sudo_plus,
+from Akenobot.modules.helper_funcs.chat_status import (dev_plus, sudo_plus,
                                                            whitelist_plus)
-from AsukaRobot.modules.helper_funcs.extraction import extract_user
-from AsukaRobot.modules.log_channel import gloggable
+from Akenobot.modules.helper_funcs.extraction import extract_user
+from Akenobot.modules.log_channel import gloggable
 from telegram import ParseMode, TelegramError, Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 from telegram.utils.helpers import mention_html
 
 ELEVATED_USERS_FILE = os.path.join(os.getcwd(),
-                                   'AsukaRobot/elevated_users.json')
+                                   'Akenobot/elevated_users.json')
 
 
 def check_user_id(user_id: int, context: CallbackContext) -> Optional[str]:
