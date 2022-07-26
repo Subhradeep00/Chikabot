6c3dd3a8f6c545a98de721358a9af549d6aced11
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-26 09:06:52 +0530

    Add files via upload

---
+++ Akenobot/modules/alive.py
@@ -0,0 +1,68 @@
+import asyncio
+import telegram
+import os
+import requests
+import datetime
+import time
+from PIL import Image
+from io import BytesIO
+from datetime import datetime
+import random
+from telethon import events, Button, custom, version
+from Akenobot.events import register
+from Akenobot import telethn as borg, OWNER_ID, OWNER_NAME
+from Akenobot import StartTime, dispatcher
+from telethon.tl.types import ChannelParticipantsAdmins
+from pyrogram import __version__ as pyro
+
+
+edit_time = 5
+""" =======================CONSTANTS====================== """
+file1 = "https://te.legra.ph/file/ce84881970d9514a62fd4.jpg"
+file2 = "https://te.legra.ph/file/5e16d1ce34d58bca8bdde.jpg"
+file3 = "https://te.legra.ph/file/4ea17f15d61bd7df808be.jpg"
+file4 = "https://te.legra.ph/file/ce057de710c7a05ae14c4.jpg"
+""" =======================CONSTANTS====================== """
+
+START_TIME = datetime.utcnow()
+START_TIME_ISO = START_TIME.replace(microsecond=0).isoformat()
+TIME_DURATION_UNITS = (
+    ('week', 60 * 60 * 24 * 7),
+    ('day', 60 * 60 * 24),
+    ('hour', 60 * 60),
+    ('min', 60),
+    ('sec', 1)
+)
+
+async def _human_time_duration(seconds):
+    if seconds == 0:
+        return 'inf'
+    parts = []
+    for unit, div in TIME_DURATION_UNITS:
+        amount, seconds = divmod(int(seconds), div)
+        if amount > 0:
+            parts.append('{} {}{}'
+                         .format(amount, unit, "" if amount == 1 else "s"))
+    return ', '.join(parts)
+
+@register(pattern=("/alive"))
+async def hmm(yes):
+    chat = await yes.get_chat()
+    current_time = datetime.utcnow()
+    uptime_sec = (current_time - START_TIME).total_seconds()
+    uptime = await _human_time_duration(int(uptime_sec))
+    Akeno = f"• **Hey [{yes.sender.first_name}](tg://user?id={yes.sender.id}), I'm 𝒜𝓀ℯ𝓃ℴ**\n"
+    Akeno += f"• **My Uptime** - `{uptime}`\n"
+    Akeno += f"• **Telethon Version** - `{version.__version__}`\n"
+    Akeno += f"• **PTB Version** - `{telegram.__version__}`\n"
+    Akeno += f"• **Pyrogram Version** - `{pyro}`\n"
+    Akeno += f"• **My Master** - [𝔸𝕜𝕒𝕥𝕤𝕦𝕜𝕚](tg://user?id={OWNER_ID})\n\n"
+    Akeno += f"Thanks For Adding Me In {yes.chat.title}"
+    BUTTON = [[Button.url("Support Chat", "https://t.me/AkenoSupport00"), Button.url("Updates", "https://t.me/AkenoSupport0")]]
+    on = await borg.send_file(yes.chat_id, file="https://te.legra.ph/file/ce84881970d9514a62fd4.jpg",caption=Asuka, buttons=BUTTON)
+
+@register(pattern=("/repo"))
+async def repo(yes):
+    Akeno = f"**Hey [{event.sender.first_name}](tg://user?id={event.sender.id}), Click The Button Below To Get My Repo**\n\n"
+    BUTTON = [[Button.url("GitHub", "https://github.com/Subhradeep00/Akenobot"), Button.url("Developer", "https://t.me/Subhradeep00")]]
+    await borg.send_file(event.chat_id, file="https://te.legra.ph/file/ce84881970d9514a62fd4.jpg", caption=Akeno, buttons=BUTTON)
