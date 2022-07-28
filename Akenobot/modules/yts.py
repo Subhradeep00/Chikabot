2ee817dab04e6113f255e21dbdcc42f90173ace8
Subhradeep <102454741+Subhradeep00@users.noreply.github.com>
2022-07-28 08:37:50 +0530

    Update yts.py

--- Akenobot/modules/yts.py
+++ Akenobot/modules/yts.py
@@ -49,13 +49,11 @@ async def ytsearch(_, message: Message):
         i = 0
         text = ""
         while i < 6:
-            ➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
             text += f" 🎩 Title - {results[i]['title']}\n"
             text += f" ⏳️ Duration - {results[i]['duration']}\n"
             text += f" 👀 Views - {results[i]['views']}\n"
             text += f" 🖥 Channel - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
-            ➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖➖
             i += 1
         await m.edit(text, disable_web_page_preview=True)
     except Exception as e:
